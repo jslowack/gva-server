74f844b7fd86cce8d1db7cfa2188ee55b5b271d8
Jelle Slowack <jslowack@gmail.com>
2017-04-23 17:18:31 +0200

    Rotate image conform to EXIF rotation information

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ function createThumbnail(originalFile, fileName){
     console.log(fileName);
     const maxSize = 256;
     sharp(originalFile)
+        .rotate()
         .resize(maxSize, maxSize)
         .toFile(fileName);
     console.log("end");
@@ -45,11 +46,11 @@ function createThumbnail(originalFile, fileName){
 app.post('/peopleadd', upload.single('image'), async function (req, res) {
     try {
         const col = await loadCollection(COLLECTION_NAME, db);
+        const thumbName = (req.file && req.file.path) ?  req.file.filename+'thumb' : null;
         const fileName = (req.file && req.file.path) ? req.file.path+'thumb' : null;
-        console.log(req.file);
         const data = col.insert(
             { 
-                image: req.file.filename+'thumb',
+                image: thumbName,
                 userInfo: {
                     'name': req.body.name,
                     'email': req.body.email,
@@ -57,12 +58,7 @@ app.post('/peopleadd', upload.single('image'), async function (req, res) {
                 }
             }
         );
-        console.log("e");
-        
         db.saveDatabase();
-        console.log("e")
-        console.log(req.file.buffer)
-
         if (fileName)
             createThumbnail(req.file.path, fileName);
         
@@ -73,8 +69,9 @@ app.post('/peopleadd', upload.single('image'), async function (req, res) {
 })
 
 function transformPeopleCollection(req, data){
-    return data.map(userData => {
+    return data.map((userData, index) => {
         return {
+            id: userData.$loki,
             image: (userData.image) ? req.protocol + '://' + req.get('host') + '/' + UPLOAD_PATH_FOLDER + '/' + userData.image : null, 
             name: userData.userInfo.name,
             lat: userData.userInfo.geo.lat,
